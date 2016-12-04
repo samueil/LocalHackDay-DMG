20650031cd8c12df6c7a7d1d245fa7de630ecdeb
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2016-12-04 22:54:20 +0100

    Hastags are now supported

--- src/app/feedback-form/feedback-form.component.ts
+++ src/app/feedback-form/feedback-form.component.ts
@@ -1,32 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {AngularFire} from "angularfire2";
+declare var $;
 
 @Component({
   selector: 'feedback-form',
   templateUrl: './feedback-form.component.html',
   styleUrls: ['./feedback-form.component.css']
 })
-export class FeedbackFormComponent implements OnInit {
+export class FeedbackFormComponent implements OnInit, AfterViewInit {
 
   feedbackForm: FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private af:AngularFire) {
+  constructor(private formBuilder: FormBuilder, private af: AngularFire) {
 
   }
 
   ngOnInit() {
     this.feedbackForm = this.formBuilder.group({
-      title: [ '', Validators.required ],
-      text: [ '', Validators.required ],
+      title: ['', Validators.required],
+      text: ['', Validators.required],
       date: Date.now()
     })
   }
 
   submitFeedback(data) {
+    let tags = [];
+
+    $('.chips > .chip').each(function () {
+      tags.push($(this).text());
+    });
+    data.tags = tags;
+    console.log(data);
     data.vote = 0;
     this.af.database.list('/feedback').push(data);
     this.feedbackForm.reset();
+    $('.chips-placeholder').material_chip({
+      data: []
+    });
   }
+
+
+  ngAfterViewInit() {
+    $('.chips-placeholder').material_chip({
+      placeholder: 'Enter a tag',
+      secondaryPlaceholder: '+Tag',
+    });
+
+  }
+
 
 }
