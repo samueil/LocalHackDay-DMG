df13aee4ffdf59d31110baea085bbfee97f4ab55
Dorus Janssens <dorus.janssens@stud.hslu.ch>
2016-12-22 11:06:34 +0100

    Fixed Mobile Nav

--- src/app/components/navbar.component.ts
+++ src/app/components/navbar.component.ts
@@ -1,17 +1,28 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnInit, AfterViewInit} from "@angular/core";
 import {AngularFire} from "angularfire2";
 import {Router} from "@angular/router";
+declare var $;
 
 @Component({
   selector: 'navbar',
   templateUrl: '../templates/navbar.component.html',
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, AfterViewInit {
 
   constructor(public af: AngularFire, private router: Router) {
   }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    $('.button-collapse').sideNav({
+        menuWidth: 300, // Default is 240
+        edge: 'left', // Choose the horizontal origin
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+      }
+    );
   }
 
   logout() {
